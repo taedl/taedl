8b503e045f4df0191d9d38cd6ef16486ec72355c
Pavel Yaroshchyk <pavel.yaroshchyk@gmail.com>
2018-09-01 22:30:01 +1000

    server stub

---
+++ adhoc-api/src/main/java/com/erkoa/adhocapi/exceptions/FormatConversionException.java
@@ -0,0 +1,7 @@
+package com.erkoa.adhocapi.exceptions;
+
+public class FormatConversionException extends RuntimeException {
+    public FormatConversionException(String message) {
+        super(message);
+    }
+}
