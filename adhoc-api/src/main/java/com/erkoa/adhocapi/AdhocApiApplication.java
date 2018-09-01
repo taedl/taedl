0df954334f2fbdd2bb05d664ece39a45ebd19e4e
Pavel Yaroshchyk <pavel.yaroshchyk@gmail.com>
2018-09-01 21:11:56 +1000

    initial commit

---
+++ adhoc-api/src/main/java/com/erkoa/adhocapi/AdhocApiApplication.java
@@ -0,0 +1,12 @@
+package com.erkoa.adhocapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdhocApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdhocApiApplication.class, args);
+	}
+}
