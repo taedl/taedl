763995bd5ae8f278bfe76d9a54a45ff9a32d15b6
Pavel Yaroshchyk <pavel.yaroshchyk@gmail.com>
2018-09-05 23:30:51 +1000

    cleaned up broken tests

--- adhoc/src/app/connection/connection.component.spec.ts
+++ adhoc/src/app/connection/connection.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConnectionComponent } from './connection.component';
+import { MatCardModule, MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule } from '@angular/material';
+import { ConnectionsApiService } from '../services/connections-api.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ConnectionComponent', () => {
   let component: ConnectionComponent;
@@ -8,7 +13,19 @@ describe('ConnectionComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ConnectionComponent ]
+      declarations: [ ConnectionComponent ],
+      imports: [
+        MatCardModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatOptionModule,
+        MatSelectModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        BrowserAnimationsModule
+      ],
+      providers: [ ConnectionsApiService ]
     })
     .compileComponents();
   }));
--- adhoc/src/app/home/home.component.spec.ts
+++ adhoc/src/app/home/home.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { ConnectionComponent } from '../connection/connection.component';
+import { DomainComponent } from '../domain/domain.component';
+import { MatCardModule, MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +14,18 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent, ConnectionComponent, DomainComponent ],
+      imports: [
+        MatCardModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatOptionModule,
+        MatSelectModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        BrowserAnimationsModule
+      ]
     })
     .compileComponents();
   }));
